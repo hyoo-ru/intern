f43301fcd692bf0b37a75f3f7cc779e9e8c2f1b3
stan-donarise <111230475+stan-donarise@users.noreply.github.com>
2023-05-21 19:47:11 +0300

    centered layout with default page

--- intern.view.css.ts
+++ intern.view.css.ts
@@ -1,6 +1,15 @@
 namespace $.$$ {
 
+	const page_default = {
+		flex: { 
+			grow: 1,
+			basis: 'min(100%, 60rem)' as any,
+		},
+	}
+
 	$mol_style_define( $hyoo_intern, {
+
+		justifyItems: 'center',
 		
 		Menu: {
 			flex: {
@@ -8,12 +17,36 @@ namespace $.$$ {
 				shrink: 0,
 			},
 			Head: {
-				justifyContent: 'flex-start',
+				justifyContent: 'center',
+				alignItems: 'center',
 				gap: $mol_gap.block,
 			},
 			Foot: {
 				padding: 0,
 			},
+		},
+
+		Label: {
+			padding: $mol_gap.block,
+		},
+
+		Register_page: page_default,
+		About_page: page_default,
+		Intern_profile: page_default,
+		Vacancies: page_default,
+		Applications: page_default,
+		Invitations: page_default,
+
+		Register: {
+			margin: {
+				top: $mol_gap.block,
+			},
+		},
+
+		Register_form: {
+			Foot: {
+				justifyContent: 'center',
+			}
 		},
 		
 		Logo: {
--- intern.view.ts
+++ intern.view.ts
@@ -11,7 +11,33 @@ namespace $.$$ {
 		
 		@ $mol_mem
 		spreads() {
-			return this.role_spreads()[ this.role() as Role ]
+			return this.role_spreads()[ this.role() ]
+		}
+
+		arg( spread: string ) {
+			const is_default = spread === this.default_spreads()[ this.role() ]
+			return { [ this.param() ]: is_default? null : spread || null }
+		}
+
+		@ $mol_mem
+		role(next?: Role) {
+			if ( next !== undefined ) {
+				this.spread( null )
+				return next
+			}
+			return "norole"
+		}
+
+		@ $mol_mem
+		pages() {
+			const pages = super.pages()
+			const default_spread = (this.spreads() as any)[ this.default_spreads()[ this.role() ] ]
+			return [
+				this.Gap( 'left' ),
+				...pages,
+				...( pages.length === 1? [ default_spread ] : [] ),
+				this.Gap( 'right' ),
+			]
 		}
 
 	}
