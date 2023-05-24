367d5ca7cf33194d8fac6168c09e200572490d4d
stan-donarise <111230475+stan-donarise@users.noreply.github.com>
2023-05-24 13:50:48 +0300

    css

--- intern.view.css.ts
+++ intern.view.css.ts
@@ -1,8 +1,26 @@
 namespace $.$$ {
 
+	const Page = {
+		maxWidth: 'min(100%, 60rem)' as any,
+		flex: { 
+			grow: 1,
+			basis: '40rem' as any,
+		},
+		// ":last-child": {
+		// 	margin: {
+		// 		right: 'auto'
+		// 	},
+		// } as any,
+	}
+
 	$mol_style_define( $hyoo_intern, {
 
 		Menu: {
+			// ":first-child": {
+			// 	margin: {
+			// 		left: 'auto'
+			// 	},
+			// },
 			flex: {
 				basis: '20rem',
 				shrink: 0,
@@ -12,27 +30,18 @@ namespace $.$$ {
 				alignItems: 'center',
 				gap: $mol_gap.block,
 			},
-			Foot: {
-				padding: 0,
-			},
 		},
 
 		Label: {
 			padding: $mol_gap.block,
 		},
 
-		Send_resume: {
-			margin: {
-				top: $mol_gap.block,
-			},
-		},
+		Intern_profile: Page,
+		Career_school: Page,
+		Tests: Page,
+		Championship: Page,
+		Responses: Page,
 
-		Resume_form: {
-			Foot: {
-				justifyContent: 'center',
-			}
-		},
-		
 		Logo: {
 			width: '3rem',
 			height: '3rem',
