3cef7c828457880cf68edb97501b9e5902926312
stan-donarise <111230475+stan-donarise@users.noreply.github.com>
2023-05-24 13:51:46 +0300

    $hyoo_intern_catalog_scroll: init

---
+++ catalog/scroll/demo/demo.view.css.ts
@@ -0,0 +1,38 @@
+namespace $.$$ {
+
+	$mol_style_define( $hyoo_intern_catalog_scroll_demo, {
+
+		Page_table: {
+			flex: {
+				wrap: 'wrap',
+			},
+		},
+
+		Id_labeler: {
+			flex: {
+				basis: '5rem',
+			},
+			Label: {
+				padding: {
+					left: '2rem',
+				},
+			},
+		},
+		
+		Id: {
+			padding: $mol_gap.text,
+		},
+
+		Page: {
+			margin: $mol_gap.block,
+			background: {
+				color: $mol_theme.card
+			},
+			flex: {
+				direction: 'column'
+			}
+		},
+		
+	} )
+
+}
---
+++ catalog/scroll/scroll.view.css.ts
@@ -0,0 +1,35 @@
+namespace $.$$ {
+
+	$mol_style_define( $hyoo_intern_catalog_scroll, {
+		
+		Rows: {
+			flex: {
+				basis: '20rem',
+			},
+		},
+		
+		Pages: {
+			maxWidth: 'min(100%, 60rem)' as any,
+			flex: { 
+				grow: 1,
+				basis: '40rem' as any,
+			},
+		},
+
+		Page_link: {
+			background: {
+				color: $mol_theme.card
+			},
+		},
+		
+		Page_content: {
+			padding: $mol_gap.block,
+			flex: {
+				direction: 'column'
+			}
+		},
+		
+		
+	} )
+
+}
