8c26daca8ce13be673f08e88ccb0b97e1421a2dd
stan-donarise <111230475+stan-donarise@users.noreply.github.com>
2023-05-26 11:26:20 +0300

    career school, tests as md text

---
+++ cabinet/admin/admin.view.css.ts
@@ -0,0 +1,15 @@
+namespace $.$$ {
+
+	$mol_style_define( $hyoo_intern_cabinet_admin, {
+
+		Campaign: $hyoo_intern_cabinet_page_style,
+		Profile: $hyoo_intern_cabinet_page_style,
+		Statistics: $hyoo_intern_cabinet_page_style,
+		Timesheet: $hyoo_intern_cabinet_page_style,
+		Career_school: $hyoo_intern_cabinet_page_style,
+		Tests: $hyoo_intern_cabinet_page_style,
+		Championship: $hyoo_intern_cabinet_page_style,
+
+	 } )
+
+}
