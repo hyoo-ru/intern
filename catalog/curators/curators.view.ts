b8163e2ee298e4b2acb9b3e6f05616acc39f211e
stan-donarise <111230475+stan-donarise@users.noreply.github.com>
2023-05-26 20:51:16 +0300

    +1

--- catalog/curators/curators.view.ts
+++ catalog/curators/curators.view.ts
@@ -3,7 +3,7 @@ namespace $.$$ {
 	export class $hyoo_intern_catalog_curators extends $.$hyoo_intern_catalog_curators {
 
 		entity_links() {
-			return super.entity_links() as $hyoo_intern_entity_links< typeof $hyoo_intern_curator >
+			return super.entity_links() as $hyoo_intern_entity_links< typeof $hyoo_intern_person >
 		}
 
 	}
