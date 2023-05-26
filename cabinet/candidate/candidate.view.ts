744eb4217eca7b7462ae787ef57934c8c44bb7ab
stan-donarise <111230475+stan-donarise@users.noreply.github.com>
2023-05-26 11:06:47 +0300

    merge candidate in intern

--- cabinet/candidate/candidate.view.ts
+++
@@ -1,8 +0,0 @@
-namespace $.$$ {
-
-	export class $hyoo_intern_cabinet_candidate extends $.$hyoo_intern_cabinet_candidate {
-
-
-	}
-
-}
--- cabinet/intern/intern.view.ts
+++ cabinet/intern/intern.view.ts
@@ -1,7 +1,23 @@
 namespace $.$$ {
 
+	type Stages = 1 | 2 | 3 | 4 | 5
+
 	export class $hyoo_intern_cabinet_intern extends $.$hyoo_intern_cabinet_intern {
 
+		@ $mol_mem
+		spreads() {
+			return this.stage_spreads()[ this.stage() as Stages ]
+		}
+
+		@ $mol_mem
+		profile_title(): string {
+			return this.stage() > 3 ? 'Профиль стажера' : 'Профиль кандидата'
+		}
+
+		@ $mol_mem
+		stage_title() {
+			return `${this.stage()} этап стажировки`
+		}
 
 	}
 
