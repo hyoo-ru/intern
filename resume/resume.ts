933966149aa77022fa62543ea3027c3163296b78
Stefan Zerkalica <zerkalica@gmail.com>
2023-12-04 02:14:37 +0300

    $mol_view_tree2 refactor, https://github.com/hyoo-ru/mam_mol/pull/646

--- resume/resume.ts
+++ resume/resume.ts
@@ -6,42 +6,42 @@ namespace $.$$ {
 
 		@ $mol_mem
 		mentor_vacancy( ) {
-			return this.data().sub( 'vacancy', $hyoo_intern_vacancy ) ?? null
+			return this.sub( 'vacancy', $hyoo_intern_vacancy ) ?? null
 		}
 
 		@ $mol_mem
 		about( next?: string ) {
-			return this.data().sub( 'about', $hyoo_crowd_reg ).str( next )
+			return this.sub( 'about', $hyoo_crowd_reg ).str( next )
 		}
 
 		@ $mol_mem
 		status( next?: string ) {
-			return this.data().sub( 'status', $hyoo_crowd_reg ).str( next )
+			return this.sub( 'status', $hyoo_crowd_reg ).str( next )
 		}
 
 		@ $mol_mem
 		country( next?: string ) {
-			return this.data().sub( 'country', $hyoo_crowd_reg ).str( next )
+			return this.sub( 'country', $hyoo_crowd_reg ).str( next )
 		}
 
 		@ $mol_mem
 		city( next?: string ) {
-			return this.data().sub( 'city', $hyoo_crowd_reg ).str( next )
+			return this.sub( 'city', $hyoo_crowd_reg ).str( next )
 		}
 
 		@ $mol_mem
 		job_status( next?: 'working_for_hire' | 'self-employed' | 'unemployed' ) {
-			return this.data().sub( 'job_status', $hyoo_crowd_reg ).str( next )
+			return this.sub( 'job_status', $hyoo_crowd_reg ).str( next )
 		}
 
 		@ $mol_mem
 		skills( next?: string[] ) {
-			return this.data().sub( 'skills', $hyoo_crowd_list ).list( next ).map(String)
+			return this.sub( 'skills', $hyoo_crowd_list ).list( next ).map(String)
 		}
 
 		@ $mol_mem
 		jobs_node() {
-			return this.data().sub( 'jobs', $hyoo_crowd_list )
+			return this.sub( 'jobs', $hyoo_crowd_list )
 		}
 
 		@ $mol_mem
@@ -61,7 +61,7 @@ namespace $.$$ {
 
 		@ $mol_mem
 		institutions_node() {
-			return this.data().sub( 'institutions', $hyoo_crowd_list )
+			return this.sub( 'institutions', $hyoo_crowd_list )
 		}
 
 		@ $mol_mem
